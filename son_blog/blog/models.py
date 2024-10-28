bc89d2d76829075a8e24f0ca0992bb6b36422917
Maria Andreza C Santos <mandreza008@gmail.com>
2024-10-28 10:24:12 -0300

    Initial Commit

---
+++ son_blog/blog/models.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    resumo = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    created_at = models.DateField(auto_now_add=True)
